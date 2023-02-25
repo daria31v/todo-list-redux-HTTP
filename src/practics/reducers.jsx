cbeb231bb5871e8abf7d17d43925360c7e0e9d86
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-02-25 13:01:15 +0100

    Update reducers.jsx

--- src/practics/reducers.jsx
+++ src/practics/reducers.jsx
@@ -1,22 +1,22 @@
 //I step type actions
-const deposit = amount => {
-  return {
-    type: 'deposit',
-    payload: { amount },
-  };
-};
+// const deposit = amount => {
+//   return {
+//     type: 'deposit',
+//     payload: { amount },
+//   };
+// };
 //I step Reducer for actions
-function fundsReducer(state = 0, action) {
-  switch (action.type) {
-    case 'deposit':
-      return state + action.payload.amount;
+// function fundsReducer(state = 0, action) {
+//   switch (action.type) {
+//     case 'deposit':
+//       return state + action.payload.amount;
 
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
 //I step store
-const store = createStore(fundsReducer);
+// const store = createStore(fundsReducer);
 
 //II step!! interface
-<button onClick={REDUX.dispatch(deposit(100))}>OK</button>;
+//  <button onClick={REDUX.dispatch(deposit(100))}>OK</button>; 
